9dc9e997223a35c0331a1f4dbc7a03f9b5081b53
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-07 20:37:51 -0500

    removing the floors column via migration

---
+++ db/migrate/20200708013144_remove_floors_from_listings.rb
@@ -0,0 +1,5 @@
+class RemoveFloorsFromListings < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :real_estates, :floors, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_012707) do
+ActiveRecord::Schema.define(version: 2020_07_08_013144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_012707) do
     t.integer "square_feet"
     t.integer "bedrooms"
     t.integer "bathrooms"
-    t.integer "floors"
     t.string "availability"
     t.integer "price"
     t.string "address"
